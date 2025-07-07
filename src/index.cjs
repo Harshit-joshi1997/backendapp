7c23b989bb4c11455b5f20e984ef9c254c07e9af
Harshit-joshi1997 <joshiharshit291@gmail.com>
2025-07-07 23:58:11 +0530

    route added

--- src/index.cjs
+++ src/index.cjs
@@ -5,6 +5,12 @@ const app = express()
 
 const port = 4000
 
+const githubData={
+    "giturl":"http:4000",
+    "project_name":"backendData",
+    "libruary":"Reactjs"
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -19,6 +25,10 @@ app.get('/login',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
     res.send('<h2>Backend course part-1</h2>')
+})
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
